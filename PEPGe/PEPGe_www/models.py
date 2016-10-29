217083c5de928c08104569f7a9bcb85c45deee9c
Tristan Maat <tm@tlater.net>
2016-10-29 19:05:26 +0100

    Add program model

--- PEPGe/PEPGe_www/models.py
+++ PEPGe/PEPGe_www/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Program (models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    date = models.DateField()
+    start = models.DateTimeField()
+    duration = models.PositiveIntegerField()
